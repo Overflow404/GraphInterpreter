b4baa97b67a6f499d8472a4ee53083943c6fd8fd
overflow404 <gaspare.armato1@gmail.com>
2019-05-08 17:01:50 +0200

    .

--- src/main/java/command/addnode/AddNodeCommand.java
+++ src/main/java/command/addnode/AddNodeCommand.java
@@ -8,24 +8,25 @@ import graph.Graph;
 import graph.GraphStorage;
 
 public class AddNodeCommand implements Command {
-	private String graph;
-	private String node;
+    private String graph;
+    private String node;
 
-	AddNodeCommand(String graph, String node) {
-		this.graph = graph;
-		this.node = node;
+    AddNodeCommand(String graph, String node) {
+        this.graph = graph;
+        this.node = node;
     }
 
     @Override
-	public void execute(ExecutionContext context) {
-		GraphStorage storage = context.getStorage();
-		if (!storage.exists(graph)) {
-			throw new GraphNotFoundException("Graph " + graph + " does not exist!");
-		}
-		Graph graph = storage.get(this.graph);
-		if (graph.containsNode(node)) {
-			throw new NodeAlreadyExistException("Node " + node + " already exists!");
-		}
-		graph.addNode(node);
-	}
+    public void execute(ExecutionContext context) {
+        GraphStorage storage = context.getStorage();
+        if (!storage.exists(graph)) {
+            throw new GraphNotFoundException("Graph " + graph + " does not exist!");
+        }
+        Graph graph = storage.get(this.graph);
+        if (graph.containsNode(node)) {
+            throw new NodeAlreadyExistException("Node " + node + " already exists!");
+        }
+
+        graph.addNode(node);
+    }
 }
